ba7784b913524440c577d9e74981db9ab7fb3ace
gricosm <gricosm>
2024-04-25 21:54:16 +0200

    added new cards and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "blender-bold": ["BlenderPro-Bold", "sans-serif"],
+        "dinpro-bold": ["DINPro-Bold", "sans-serif"],
+        "dinpro-medium": ["DINPro-Medium", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
